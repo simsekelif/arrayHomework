5cc7d778404cbaf8f63816d8548721841f470d33
simsekelif <kahyaelif@gmail.com>
2024-10-27 09:23:03 +0100

    added comment

--- array.js
+++ array.js
@@ -1,3 +1,6 @@
+// Given: An array with fruits: "banana", "apple", "strawberry", "peach", "cranberry", "strawberry", "raspberry”, “apple”
+//Check, if there are elements (strings) longer than 8 chars and print out the result
+
 let fruits = [ 'banana', 'apple', 'strawberry', 'peach', 'cranberry', 'strawberry', 'raspberry', 'apple']
 
 const moreThanEight = fruits.filter (fruits => fruits.length > 8);
