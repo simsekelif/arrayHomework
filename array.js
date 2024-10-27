7eed5c56abae18c412a99a47194569d35ac19e85
simsekelif <kahyaelif@gmail.com>
2024-10-27 09:10:27 +0100

    used .some method to check the character length

---
+++ array.js
@@ -0,0 +1,6 @@
+let fruits = [ 'banana', 'apple', 'strawberry', 'peach', 'cranberry', 'strawberry', 'raspberry', 'apple']
+
+const moreThanEight = fruits.some (fruits => fruits.length > 8);
+
+console.log(moreThanEight);
+
